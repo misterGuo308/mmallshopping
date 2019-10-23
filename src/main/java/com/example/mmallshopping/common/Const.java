b260534fac91d36e68079717d94748899dd2a9d4
guoyou <guoyou5378@139.com>
2019-10-24 00:57:44 +0800

    用户登录接口

--- src/main/java/com/example/mmallshopping/common/Const.java
+++ src/main/java/com/example/mmallshopping/common/Const.java
@@ -6,12 +6,5 @@ package com.example.mmallshopping.common;
  */
 public class Const {
 
-    public static final String CURRENT_USER = "currentUser";
-    public static final String EMAIL = "email";
-    public static final String USERNAME = "username";
-
-    public interface role {
-        int ROLE_CUSTOMER = 0;//普通用户
-        int ROLE_ADMIN = 1;//管理员
-    }
+    public static  final  String CURRENT_USER ="currentUser";
 }
--- src/main/java/com/example/mmallshopping/controller/portal/UserController.java
+++ src/main/java/com/example/mmallshopping/controller/portal/UserController.java
@@ -1,7 +1,6 @@
 package com.example.mmallshopping.controller.portal;
 
 import com.example.mmallshopping.common.Const;
-import com.example.mmallshopping.common.ResponseCode;
 import com.example.mmallshopping.common.ServerResponse;
 import com.example.mmallshopping.pojo.User;
 import com.example.mmallshopping.service.UserService;
@@ -24,7 +23,6 @@ public class UserController {
 
     /**
      * 用户登录
-     *
      * @param username
      * @param password
      * @param session
@@ -39,85 +37,5 @@ public class UserController {
         return response;
     }
 
-    @PostMapping("/logout")
-    public ServerResponse<String> logout(HttpSession session) {
-        session.removeAttribute(Const.CURRENT_USER);
-        return ServerResponse.createBySuccess();
-    }
 
-    @PostMapping("/register")
-    public ServerResponse<String> register(User user) {
-        return userService.register(user);
-    }
-
-    @PostMapping("/checkValid")
-    public ServerResponse<String> checkValid(String str, String type) {
-        return userService.checkValid(str, type);
-    }
-
-    @PostMapping("/get_user_info")
-    public ServerResponse<User> getUserInfo(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user != null) {
-            return ServerResponse.createBySuccess(user);
-        }
-        return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
-    }
-
-    @PostMapping("/forget_get_question")
-    public ServerResponse<String> forgetGetQuestion(String username) {
-        return userService.selectQuestion(username);
-    }
-
-    @PostMapping("/forget_check_answer")
-    public ServerResponse<String> forgetCheckAnswer(String username, String question, String answer) {
-        return userService.checkAnswer(username, question, answer);
-    }
-
-    /**
-     * 重置密码
-     *
-     * @param username
-     * @param passwordNew
-     * @param forgetToken
-     * @return
-     */
-    @PostMapping("/forget_reset_password")
-    public ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken) {
-        return userService.forgetResetPassword(username, passwordNew, forgetToken);
-    }
-
-    @PostMapping("/reset_password")
-    public ServerResponse<String> resetPassword(String passwordOld, String passwordNew, HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user == null) {
-            return ServerResponse.createByErrorMessage("用户未登录,请先登录本系统");
-        }
-        return userService.resetPassword(passwordOld, passwordNew, user);
-    }
-
-    @PostMapping("/update_information")
-    public ServerResponse<User> updateInformation(User user, HttpSession session) {
-        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser == null) {
-            return ServerResponse.createByErrorMessage("用户未登录,请先登录本系统");
-        }
-        //用户名称不允许修改
-        user.setUsername(currentUser.getUsername());
-        user.setId(currentUser.getId());
-        ServerResponse<User> response = userService.updateInformation(user);
-        if (response.isSuccess()) {
-            response.getData().setUsername(currentUser.getUsername());
-            session.setAttribute(Const.CURRENT_USER, response.getData());
-        }
-        return response;
-    }
-    @PostMapping("/get_information")
-    public ServerResponse<User>getInformation(HttpSession session ){
-        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser == null) {
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登录,需要强制登录status=10");
-        }
-       return userService.getInformation(currentUser.getId());
-    }
 }
