7b1b5abbf80cefe06fb02e5ff1f40b05b5899386
gxder <guoyou5378@126.com>
2019-10-24 10:31:04 +0800

    Revert "用户登录接口"
    This reverts commit b260534f

--- src/main/java/com/example/mmallshopping/service/UserService.java
+++ src/main/java/com/example/mmallshopping/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.mmallshopping.service;
 
 import com.example.mmallshopping.common.ServerResponse;
+import com.example.mmallshopping.pojo.User;
 
 /**
  * @author guoyou
@@ -8,4 +9,20 @@ import com.example.mmallshopping.common.ServerResponse;
  */
 public interface UserService {
     ServerResponse login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse<String> selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+
+    ServerResponse<User> updateInformation(User user);
+
+    ServerResponse<User> getInformation(Integer id);
 }
